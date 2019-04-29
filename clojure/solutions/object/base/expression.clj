aae04adad93e7a8858fd660feedb3c5b95860c83
Никита Елфимов <47705038+EasySocHacks@users.noreply.github.com>
2019-04-29 23:13:37 +0300

    Create expression.clj

---
+++ clojure/solutions/object/base/expression.clj
@@ -0,0 +1,85 @@
+(declare ZERO)
+(declare ONE)
+
+(defn evaluate [expression varsValue] ((.evaluate expression) varsValue))
+(defn toString [expression] (.castString expression))
+(defn diff [expression diffVariable] ((.diff expression) diffVariable))
+
+(definterface IOperation
+  (evaluate [])
+  (castString [])
+  (diff []))
+
+(deftype ConstantOperation [constValue]
+  IOperation
+  (evaluate [this] (fn [_] constValue))
+  (castString [this] (format "%.1f" (double constValue)))
+  (diff [this] (fn [_] ZERO)))
+
+(defn Constant [constValue] (ConstantOperation. constValue))
+
+(def ZERO (Constant 0))
+(def ONE (Constant 1))
+
+(deftype VariableOperation [varName]
+  IOperation
+  (evaluate [this] (fn [varsValue] (varsValue varName)))
+  (castString [this] varName)
+  (diff [this] (fn [diffVariable] (if (= diffVariable varName) ONE ZERO))))
+
+(defn Variable [varName] (VariableOperation. varName))
+
+(deftype CommonOperation [comparator stringOperation diffComparator expressions]
+  IOperation
+  (evaluate [this] (fn [varsValue] (apply comparator (map (fn [expression] (evaluate expression varsValue)) expressions))))
+  (castString [this] (str "(" stringOperation " " (clojure.string/join " " (map (fn [expression] (toString expression)) expressions)) ")"))
+  (diff [this] diffComparator))
+
+(defn Add [& expressions] (CommonOperation.
+  +
+  "+"
+  (fn [diffVariable] (Add (diff (nth expressions 0) diffVariable) (diff (nth expressions 1) diffVariable)))
+  expressions))
+
+(defn Subtract [& expressions] (CommonOperation.
+  -
+  "-"
+  (fn [diffVariable] (Subtract (diff (nth expressions 0) diffVariable) (diff (nth expressions 1) diffVariable)))
+  expressions))
+
+(defn Multiply [& expressions] (CommonOperation.
+  *
+  "*"
+  (fn [diffVariable] (Add (Multiply (diff (nth expressions 0) diffVariable) (nth expressions 1))
+                          (Multiply (nth expressions 0) (diff (nth expressions 1) diffVariable))))
+  expressions))
+
+
+(defn Divide [& expressions] (CommonOperation.
+  (fn [a b] (/ (double a) (double b)))
+  "/"
+  (fn [diffVariable] (Divide (Subtract (Multiply (diff (nth expressions 0) diffVariable) (nth expressions 1))
+                                       (Multiply (diff (nth expressions 1) diffVariable) (nth expressions 0)))
+                             (Multiply (nth expressions 1) (nth expressions 1))))
+  expressions))
+
+(defn Negate [expression] (CommonOperation.
+  -
+  "negate"
+  (fn [diffVariable] (Negate (diff expression diffVariable)))
+  (list expression)))
+
+(def getOperation {
+  '+ Add '- Subtract '* Multiply '/ Divide 'negate Negate
+  })
+
+(defn parse [expression]
+  (cond
+    (seq? expression) (apply (getOperation (first expression)) (map parse (rest expression)))
+
+    (number? expression) (Constant expression)
+
+    :else (Variable (str expression))
+    ))
+
+(defn parseObject [expressionString] (parse (read-string expressionString)))
